f4672d78933c1cc773c89f0bdb1595fefd2ef05b
Kunal Das <kunal.d@purplle.com>
2020-09-26 13:24:28 +0530

    Initial Commit

---
+++ MobileAxxess_CodeChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileAxxess_CodeChallenge
+//
+//  Created by Kunal Das on 26/09/20.
+//  Copyright © 2020 Kunal Das. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
